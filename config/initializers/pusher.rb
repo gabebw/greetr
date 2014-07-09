c332ce570de3d015cab49cf0e97c8a5c2cc5b1d9
Gabe Berke-Williams <gabebw@gabebw.com>
2014-07-09 10:46:31 -0400

    Extract Pusher keys to env variables

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,2 @@
-# config/initializers/pusher.rb
-Pusher.url = "http://880e597cf4a3b4d64c95:37f9a9fed638d6db7dc6@api.pusherapp.com/apps/80859"
+Pusher.url = ENV["PUSHER_URL"]
 Pusher.logger = Rails.logger
