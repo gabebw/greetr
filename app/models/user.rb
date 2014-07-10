021223e5fe2223f84182b088c1ef3759f7be10f7
Gabe Berke-Williams <gabebw@gabebw.com>
2014-07-09 22:33:31 -0400

    Destroy greetings when user is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
-  has_many :greetings, foreign_key: :sender_id
+  has_many :greetings, foreign_key: :sender_id, dependent: :destroy
 
   def self.except(user)
     where.not(id: user.id)
