85951bdd720af91c738c2c1d83f6cf216de0b33c
Gabe Berke-Williams <gabebw@gabebw.com>
2014-07-10 15:11:34 -0400

    Validate presence of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 
   has_many :greetings, foreign_key: :sender_id, dependent: :destroy
 
