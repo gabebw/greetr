c4914b5e8920cd44c69964969f0c8d4136c65fac
Gabe Berke-Williams <gabebw@gabebw.com>
2014-07-13 17:57:43 -0400

    Extracting this just made the code harder to read
    The benefits were minimal, and made code readers look in 2 places rather than 1.
    
    This reverts commit b956f1ec72f151783f078cc2b7533bb2b0bd09f3.

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -2,6 +2,19 @@ $(function(){
   var pusher = new Pusher(window.PUSHER_KEY, { authEndpoint: '/pusher_authentication' });
   var channel = pusher.subscribe(window.PUSHER_CHANNEL_NAME);
 
-  channel.bind('sent', PusherCallbacks.onSent);
-  channel.bind('new_user', PusherCallbacks.onNewUser)
+  channel.bind('sent', function(data){
+    var message = data.message;
+    var newGreeting = $("<div>").css({class: "greeting"}).html(message).hide();
+
+    newGreeting.find("abbr.timeago").timeago();
+    $("#greetings").prepend(newGreeting);
+    titleNotification.increaseNumberOfNotifications();
+    highlight(newGreeting);
+  });
+
+  channel.bind('new_user', function(data){
+    var user = data.message;
+    var newOption = $("<option>").prop("value", user.id).text(user.username);
+    $("#greeting_receiver_id").append(newOption);
+  });
 });
